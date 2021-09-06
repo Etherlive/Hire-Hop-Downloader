89a12690d13b94a71f37d9569360d6f02506ab10
Oscar Davies <odavies@etherlive.co.uk>
2021-09-06 11:07:17 +0100

    Init Proj

---
+++ Hire-Hop-Downloader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Hire_Hop_Interface.Management;
+using Hire_Hop_Interface.Requests;
+
+namespace Hire_Hop_Downloader
+{
+    class Program
+    {
+        static ClientConnection myHHConn = new ClientConnection();
+
+        static void Main(string[] args)
+        {
+            App();
+            while (true)
+            {
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+
+        static async void App()
+        {
+            
+        }
+    }
+}
