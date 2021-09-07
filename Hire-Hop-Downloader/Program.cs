f599ebbe915c8aa06a03aa698450fe3a28ecaad1
Oscar Davies <oscar.william.davies@outlook.com>
2021-09-07 09:38:03 +0100

    GetLogin from File

--- Hire-Hop-Downloader/Program.cs
+++ Hire-Hop-Downloader/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 using Hire_Hop_Interface.Management;
 using Hire_Hop_Interface.Requests;
 using Newtonsoft.Json.Linq;
@@ -18,15 +19,43 @@ namespace Hire_Hop_Downloader
             }
         }
 
+        static void GetLogin(out string username, out string password)
+        {
+            if (File.Exists("./login.json"))
+            {
+                string content = File.ReadAllText("./login.json");
+                JObject login = JObject.Parse(content);
+
+                username = login["username"].ToString();
+                password = login["password"].ToString();
+
+                Console.WriteLine("Loaded details from file");
+            }
+            else
+            {
+                Console.WriteLine("Enter Username: ");
+                username = Console.ReadLine();
+                Console.WriteLine("Enter Password: ");
+                password = Console.ReadLine();
+
+                JObject login = new JObject();
+                login["username"] = username;
+                login["password"] = password;
+                File.WriteAllText("./login.json", login.ToString());
+            }
+        }
+
         static async void App()
         {
-            
+            GetLogin(out string username, out string password);
 
-            bool loggedin = await Authentication.Login(myHHConn, "odavies@etherlive.co.uk", "");
+            bool loggedin = await Authentication.Login(myHHConn, username, password);
 
             if (loggedin)
             {
                 JObject job = await Jobs.GetJobData(myHHConn, "1131");
+
+                Console.WriteLine($"Loaded job {job["ID"]}");
             }
         }
     }
